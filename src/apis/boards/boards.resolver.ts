249dda971d9775a8cd652dfbc93253f060c7eb03
npark11 <nari36@naver.com>
2023-11-25 19:47:19 -0500

    create fetchBoards, createBoard

--- src/apis/boards/boards.resolver.ts
+++ src/apis/boards/boards.resolver.ts
@@ -1,5 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { BoardsService } from './boards.service';
+import { Board } from './entities/board.entity';
 
 @Resolver()
 export class BoardsResolver {
@@ -7,8 +8,17 @@ export class BoardsResolver {
     private readonly boardsService: BoardsService, //
   ) {}
 
-  @Query(() => String, { nullable: true })
-  fetchBoards(): string {
-    return this.boardsService.getHello();
+  @Query(() => [Board], { nullable: true })
+  fetchBoards(): Board[] {
+    return this.boardsService.findAll();
+  }
+
+  @Mutation(() => String)
+  createBoard(
+    @Args('writer') writer: string,
+    @Args('title') title: string,
+    @Args({ name: 'contents', nullable: true }) contents: string,
+  ): string {
+    return this.boardsService.create(writer, title, contents);
   }
 }
--- src/apis/boards/boards.service.ts
+++ src/apis/boards/boards.service.ts
@@ -1,8 +1,38 @@
 import { Injectable, Scope } from '@nestjs/common';
+import { Board } from './entities/board.entity';
 
 @Injectable({ scope: Scope.DEFAULT })
 export class BoardsService {
-  getHello(): string {
-    return 'Hello World!';
+  findAll(): Board[] {
+    const result = [
+      {
+        number: 1,
+        writer: 'Tom',
+        title: 'Title01',
+        contents: 'Contents01',
+      },
+      {
+        number: 2,
+        writer: 'Olivia',
+        title: 'Title02',
+        contents: 'Contents02',
+      },
+      {
+        number: 3,
+        writer: 'Jack',
+        title: 'Title03',
+        contents: 'Contents03',
+      },
+    ];
+
+    return result;
+  }
+
+  create(writer: string, title: string, contents: string): string {
+    console.log(writer);
+    console.log(title);
+    console.log(contents);
+
+    return 'Created your post Successfully!';
   }
 }
