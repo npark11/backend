0c69a93931b1e803390c846da4b9ed244e988e9e
npark11 <nari36@naver.com>
2023-12-02 16:14:21 -0500

    finished create, fetch products

--- src/apis/products/products.resolver.ts
+++ src/apis/products/products.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateProductInput } from './dto/create-product.input';
 import { Product } from './entities/product.entity';
 import { ProductsService } from './products.service';
@@ -8,6 +8,18 @@ export class ProductsResolver {
   constructor(
     private readonly productsService: ProductsService, //
   ) {}
+
+  @Query(() => [Product])
+  fetchProducts(): Promise<Product[]> {
+    return this.productsService.findAll();
+  }
+
+  @Query(() => Product)
+  fetchProduct(
+    @Args('productId') productId: string, //
+  ): Promise<Product> {
+    return this.productsService.findOne({ productId });
+  }
 
   @Mutation(() => Product)
   createProduct(
--- src/apis/products/products.service.ts
+++ src/apis/products/products.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
-import { IProductsServiceCreate } from './interfaces/products-service.interface';
+import {
+  IProductsServiceCreate,
+  IProductsServiceFindOne,
+} from './interfaces/products-service.interface';
 
 @Injectable()
 export class ProductsService {
@@ -10,6 +13,14 @@ export class ProductsService {
     @InjectRepository(Product)
     private readonly productsRepository: Repository<Product>, //
   ) {}
+
+  findAll(): Promise<Product[]> {
+    return this.productsRepository.find();
+  }
+
+  findOne({ productId }: IProductsServiceFindOne): Promise<Product> {
+    return this.productsRepository.findOne({ where: { id: productId } });
+  }
 
   create({ createProductInput }: IProductsServiceCreate): Promise<Product> {
     const result = this.productsRepository.save({
