5207c495e0006e4a0541b7696037fcee1e83ee77
npark11 <nari36@naver.com>
2023-12-02 12:37:26 -0500

    added product service

---
+++ src/apis/products/products.resolver.ts
@@ -0,0 +1,18 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateProductInput } from './dto/create-product.input';
+import { Product } from './entities/product.entity';
+import { ProductsService } from './products.service';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(
+    private readonly productsService: ProductsService, //
+  ) {}
+
+  @Mutation(() => Product)
+  createProduct(
+    @Args('createProductInput') createProductInput: CreateProductInput,
+  ): Promise<Product> {
+    return this.productsService.create({ createProductInput });
+  }
+}
---
+++ src/apis/products/products.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { IProductsServiceCreate } from './interfaces/products-service.interface';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
+
+  create({ createProductInput }: IProductsServiceCreate): Promise<Product> {
+    const result = this.productsRepository.save({
+      ...createProductInput,
+    });
+    return result;
+  }
+}
